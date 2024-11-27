69cac647ddbd166a0f0c46814888156d23d66e19
sahanamohankumar <sahanamohankumar23@gmail.com>
2024-11-27 18:19:11 -0500

    Implement lambda for sums

--- takeHomeProjectCapstone/src/Main.java
+++ takeHomeProjectCapstone/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int start = Integer.parseInt(args[0]);
+        int end = Integer.parseInt(args[1]);
+
+        Sum sum = (a, b) -> {
+            int total = 0;
+            for (int i = a; i <= b; i++) {
+                total += i;
+            }
+            return total;
+        };
+        System.out.println( sum.total(start, end));
+
     }
 }
--- takeHomeProjectCapstone/src/Sum.java
+++ takeHomeProjectCapstone/src/Sum.java
@@ -1,4 +1,4 @@
 @FunctionalInterface
 public interface Sum {
-    int sum(int start, int end);
+    int total(int start, int end);
 }
